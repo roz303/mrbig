e209dce83b0b1609e279247e50a9a2b3fbf6e2ab
roz303 <476f61@gmail.com>
2018-10-29 14:16:32 -0500

    Update updateusers.py

--- updateusers.py
+++ updateusers.py
@@ -4,14 +4,9 @@ from telethon.tl.types import ChannelParticipantsSearch
 from time import sleep
 import os
 
-#736474925:AAEKWsWHYz3QmHN6AZPTSc2b6UoREY26ZTI
-#736474925:AAEKWsWHYz3QmHN6AZPTSc2b6UoREY26ZTI
-
-# These example values won't work. You must get your own api_id and
-# api_hash from https://my.telegram.org, under API Development.
-api_id = 503337
-api_hash = 'b8f30e5aa88d3ed046bdf2ab128f988c'
-groupname = 'https://t.me/joinchat/G7DhT0jQUZPViOXZWDUMAw'
+api_id = 1337
+api_hash = 'YOUR API HASH'
+groupname = 'YOUR GROUP NAME'
 client = TelegramClient('userlist_update', api_id, api_hash)
 try:
 	client.start()
@@ -28,10 +23,10 @@ try:
 		all_participants.extend(participants.users)
 		offset += len(participants.users)
 finally:
-	if(os.path.isfile("/home/roswell/telegramusers.txt")):
-		os.remove("/home/roswell/telegramusers.txt")
+	if(os.path.isfile("/path/to/user/list")):
+		os.remove("/path/to/user/list")
 
-	with open("/home/roswell/telegramusers.txt", "w+") as f:
+	with open("/path/to/user/list", "w+") as f:
 		for user in all_participants:
 			writeString = user.username + "\n"
 			f.write(writeString)
